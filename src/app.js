c7c4b4dc17bafb61949eb422b902461591c9d3b9
bowotojerry <bowotojeremiah@shanksmediaandsoftwarecompany.com>
2025-10-16 21:09:56 +0100

    feat: added error handler middleware

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 const express = require("express");
 const { logger, morganStream } = require("../src/modules/utils/logger");
 const morgan = require("morgan");
+const globalErrorHandler = require('../src/modules/controllers/error-controller')
 const app = express();
 
 //express-inbuilt middleware
 app.use(express.json()); // to parse request body data
 app.use(express.urlencoded({ extended: true })); // to parse form data
-// Use Morgan middleware with a predefined format, piping logs to Winston
+// Use Morgan middleware, piping logs to Winston
 app.use(morgan("combined", { stream: morganStream }));
 
 
-// 4. Catch-all route for 404 errors
-// app.use('*', (req, res) => {
-//   res.status(404).json({
-//     error: 'Not Found',
-//     message: `The requested resource ${req.originalUrl} was not found on the server`,
-//   });
-// });
+// 4. CATCH-ALL 404 route handler
+// app.all("*", (req, res, next) => {
+//   next(new AppError(`Cant find ${req.originalUrl} on this server`, 404))
+// })
 
-// global error handling
+
+// 5. Error handling middleware
+app.use(globalErrorHandler);
 
 module.exports = app;
