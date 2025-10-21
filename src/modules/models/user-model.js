7d46457ee377c00195139ba5420e3cf5a763302d
bowotojerry <bowotojeremiah@shanksmediaandsoftwarecompany.com>
2025-10-21 12:00:19 +0100

    feat: added the user data model, also helmet for setting security headers

--- src/modules/models/user-model.js
+++ src/modules/models/user-model.js
@@ -2,37 +2,39 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 const validator = require('validator');
 
-const userSchema = new mongoose.Schema({
-  fullName: {
-    type: String,
-    required: [true, 'full name is required'],
-  },
-  email: {
-    type: String,
-    required: [true, 'email is required'],
-    unique: [true, 'email already exists'], //This creates an index to ensure email uniqueness
-    lowercase: true,
-    validate: [validator.isEmail, ' please provide a valid email'],
-  },
-  password: {
-    type: String,
-    required: [true, 'password is required'],
-    minLength: 8,
-    select: false, //exclude password from query outputs and allows projection
-  },
-  confirmPassword: {
-  validate: {
-    validator: function (el) {
-      return el === this.password;
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: [true, 'full name is required'],
     },
-    message: 'Passwords are not the same!',
+    email: {
+      type: String,
+      required: [true, 'email is required'],
+      unique: [true, 'email already exists'], //This creates an index to ensure email uniqueness
+      lowercase: true,
+      validate: [validator.isEmail, ' please provide a valid email'],
+    },
+    password: {
+      type: String,
+      required: [true, 'password is required'],
+      minLength: 8,
+      select: false, //exclude password from query outputs and allows projection
+    },
+    confirmPassword: {
+      validate: {
+        validator: function (el) {
+          return el === this.password;
+        },
+        message: 'Passwords are not the same!',
+      },
+    },
+    companyName: {
+      type: String,
+    },
   },
-},
-  companyName: {
-    type: String,
-  },
-},
-  { timestamps: true });
+  { timestamps: true }
+);
 
 // pre-save middleware to hash password
 userSchema.pre('save', async function (next) {
@@ -51,11 +53,11 @@ userSchema.methods.correctPassword = async function (
 };
 
 //virtual property to return ID as string,useful on the frontend
-userSchema.virtual("id").get(function () {
+userSchema.virtual('id').get(function () {
   return this._id.toHexString();
 });
 
-userSchema.set("toJSON", {
+userSchema.set('toJSON', {
   virtuals: true,
 });
 
